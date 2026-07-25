9b6ff94f40f7f479c9b47a6b4a2cd64cbd53a1a7
Zuhair Shad <123764183+Zuhairshad@users.noreply.github.com>
2026-07-26 00:29:21 +0500

    revert(fonts): remove external CDN link to restore original Next.js font variable scoping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,14 +62,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${cormorant.variable} ${montserrat.variable} ${jetbrains.variable} ${mrDeHaviland.variable}`}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Montserrat:ital,wght@0,100..700;1,100..700&family=Mr+De+Haviland&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
